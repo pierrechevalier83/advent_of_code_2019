b5a095dba998ffa4a3674dea4a254a5d665b5b59
Pierre Chevalier <pierrechevalier83@gmail.com>
2019-12-05 19:40:31 +0000

    02: part 1: build intcode computer

---
+++ 02/src/main.rs
@@ -0,0 +1,122 @@
+fn parse_input() -> Vec<usize> {
+    let data = include_str!("input.txt");
+    data.split(|c| c == '\n' || c == ',')
+        .filter_map(|s| s.parse().ok())
+        .collect()
+}
+
+#[derive(Eq, PartialEq)]
+enum Operation {
+    Add,
+    Multiply,
+    End,
+}
+
+impl Operation {
+    fn from_code(code: usize) -> Result<Operation, String> {
+        match code {
+            1 => Ok(Operation::Add),
+            2 => Ok(Operation::Multiply),
+            99 => Ok(Operation::End),
+            _ => Err(format!("Invalid operation: {}", code)),
+        }
+    }
+    fn apply(&self, computer: &mut Computer) {
+        match self {
+            Operation::Add => {
+                computer.add();
+            }
+            Operation::Multiply => {
+                computer.multiply();
+            }
+            Operation::End => (),
+        }
+    }
+}
+
+struct Computer {
+    data: Vec<usize>,
+    index: usize,
+}
+
+impl Computer {
+    fn from_data(data: Vec<usize>) -> Self {
+        Self { data, index: 0 }
+    }
+    fn apply<F>(&mut self, f: F)
+    where
+        F: Fn(usize, usize) -> usize,
+    {
+        let store_index = self.data[self.index + 3].clone();
+        self.data[store_index] = f(
+            self.data[self.data[self.index + 1]],
+            self.data[self.data[self.index + 2]],
+        );
+    }
+    fn add(&mut self) {
+        self.apply(|x, y| x + y);
+    }
+    fn multiply(&mut self) {
+        self.apply(|x, y| x * y);
+    }
+    fn next(&mut self) {
+        self.index += 4;
+    }
+    fn compute(&mut self) -> Result<(), String> {
+        let mut op = Operation::from_code(self.data[self.index])?;
+        while op != Operation::End {
+            op.apply(self);
+            self.next();
+            op = Operation::from_code(self.data[self.index])?;
+        }
+        Ok(())
+    }
+}
+
+/// Once you have a working computer, the first step is to restore the gravity assist program (your
+/// puzzle input) to the "1202 program alarm" state it had just before the last computer caught fire. To do this, before running the program, replace position 1 with the value 12 and replace position 2 with the value 2.
+fn restore_gravity_assist(computer: &mut Computer) {
+    computer.data[1] = 12;
+    computer.data[2] = 2;
+}
+
+/// What value is left at position 0 after the program halts?
+fn main() {
+    let mut computer = Computer::from_data(parse_input());
+    restore_gravity_assist(&mut computer);
+    computer.compute().unwrap();
+    println!("{}", computer.data[0])
+}
+
+mod tests {
+    #[test]
+    fn test_computer() {
+        let mut test_cases = Vec::new();
+        test_cases.push((vec![1, 0, 0, 0, 99], vec![2, 0, 0, 0, 99]));
+        // (1 + 1 = 2)
+
+        test_cases.push((vec![2, 3, 0, 3, 99], vec![2, 3, 0, 6, 99]));
+        // (3 * 2 = 6).
+
+        test_cases.push((vec![2, 4, 4, 5, 99, 0], vec![2, 4, 4, 5, 99, 9801]));
+        // (99 * 99 = 9801).
+
+        test_cases.push((
+            vec![1, 1, 1, 4, 99, 5, 6, 0, 99],
+            vec![30, 1, 1, 4, 2, 5, 6, 0, 99],
+        ));
+        // (1 + 1 = 2)
+        // (5 * 6 = 30)
+
+        test_cases.push((
+            vec![1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50],
+            vec![3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50],
+        ));
+
+        for (input, output) in test_cases {
+            let mut computer = super::Computer::from_data(input);
+            computer.compute().unwrap();
+            assert_eq!(output, computer.data);
+        }
+    }
+}
