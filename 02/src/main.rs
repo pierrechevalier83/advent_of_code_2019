315c7c201d87bbf78c0a0f423d12bdea22765da5
Pierre Chevalier <pierrechevalier83@gmail.com>
2019-12-05 20:01:59 +0000

    02: part 2: Get the inputs that give a certain output

--- 02/src/main.rs
+++ 02/src/main.rs
@@ -76,21 +76,33 @@ impl Computer {
 
 /// Once you have a working computer, the first step is to restore the gravity assist program (your
 /// puzzle input) to the "1202 program alarm" state it had just before the last computer caught fire. To do this, before running the program, replace position 1 with the value 12 and replace position 2 with the value 2.
-fn restore_gravity_assist(computer: &mut Computer, x: usize, y: usize) {
-    computer.data[1] = x;
-    computer.data[2] = y;
+fn restore_gravity_assist(computer: &mut Computer, noun: usize, verb: usize) {
+    computer.data[1] = noun;
+    computer.data[2] = verb;
 }
 
-fn compute_from_inputs(mut computer: Computer, x: usize, y: usize) -> usize {
-    restore_gravity_assist(&mut computer, x, y);
-    computer.compute().unwrap();
-    computer.data[0]
+fn compute_from_inputs(mut computer: Computer, noun: usize, verb: usize) -> Result<usize, String> {
+    restore_gravity_assist(&mut computer, noun, verb);
+    computer.compute()?;
+    Ok(computer.data[0])
 }
 
 /// What value is left at position 0 after the program halts?
 fn main() {
     let computer = Computer::from_data(parse_input());
-    println!("part 1: {}", compute_from_inputs(computer.clone(), 12, 2));
+    println!(
+        "part 1: {}",
+        compute_from_inputs(computer.clone(), 12, 2).unwrap()
+    );
+    for noun in 0..99 {
+        for verb in 0..99 {
+            if compute_from_inputs(computer.clone(), noun, verb) == Ok(19690720) {
+                println!("part 2: {}", 100 * noun + verb);
+                return;
+            }
+        }
+    }
+    panic!("Error: we didn't find a solution for part 2");
 }
 
 mod tests {
