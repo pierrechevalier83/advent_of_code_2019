d473486f1acf19c326f76b1cf7e9cdd9d4197306
Pierre Chevalier <pierrechevalier83@gmail.com>
2019-12-06 04:10:39 +0000

    04: part 2: only exactly two adjacent digits count

--- 04/src/main.rs
+++ 04/src/main.rs
@@ -12,14 +12,31 @@ fn two_adjacent_digits_are_the_same(x: Number) -> bool {
     x.digits.windows(2).any(|chunk| chunk[0] == chunk[1])
 }
 
+fn exactly_two_adjacent_digits_are_the_same(x: Number) -> bool {
+    // There will be no zero in any valid number.
+    // By adding these two zeros around the digits, the edge cases
+    // require no special treatment.
+    let mut digits_with_edges = vec![0];
+    digits_with_edges.extend_from_slice(&x.digits);
+    digits_with_edges.push(0);
+    digits_with_edges
+        .windows(4)
+        .any(|chunk| chunk[0] != chunk[1] && chunk[1] == chunk[2] && chunk[2] != chunk[3])
+}
+
 fn is_possible_password(candidate: Number) -> bool {
-    digits_are_sorted(candidate) && two_adjacent_digits_are_the_same(candidate)
+    digits_are_sorted(candidate)
+        && match candidate.mode {
+            Mode::AnyTwoAdjacentDigits => two_adjacent_digits_are_the_same(candidate),
+            Mode::ExactlyTwoAdjacentDigits => exactly_two_adjacent_digits_are_the_same(candidate),
+        }
 }
 
 const NUM_DIGITS: usize = 6;
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 struct Number {
     digits: [u8; NUM_DIGITS],
+    mode: Mode,
 }
 
 impl Number {
@@ -42,6 +59,7 @@ impl Into<Number> for u32 {
                 (self % 100 / 10) as u8,
                 (self % 10) as u8,
             ],
+            mode: Mode::AnyTwoAdjacentDigits,
         }
     }
 }
@@ -72,15 +90,29 @@ impl Iterator for Number {
     }
 }
 
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+enum Mode {
+    AnyTwoAdjacentDigits,
+    ExactlyTwoAdjacentDigits,
+}
+
 fn main() {
     //input range 248345-746315
     // * It is a six-digit number.
     // * The value is within the range given in your puzzle input.
     // * Two adjacent digits are the same (like 22 in 122345).
     // * Going from left to right, the digits never decrease; they only ever increase or stay the same (like 111123 or 135679).
-    let start: Number = 248345.into();
+    let start: u32 = 248345;
     let end: u32 = 746315;
-
-    let count = start.take_while(|n| *n < end).count();
-    println!("part 1: {}", count)
+    {
+        let start: Number = start.into();
+        let count = start.take_while(|n| *n < end).count();
+        println!("part 1: {}", count)
+    }
+    {
+        let mut start: Number = start.into();
+        start.mode = Mode::ExactlyTwoAdjacentDigits;
+        let count = start.take_while(|n| *n < end).count();
+        println!("part 2: {}", count)
+    }
 }
