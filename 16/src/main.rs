d5700a6c2fada4eefe339fdc15ce73d20274abb2
Pierre Chevalier <pierrechevalier83@gmail.com>
2019-12-22 02:22:36 +0000

    16: part 2: exploit particular case to optimize large input

--- 16/src/main.rs
+++ 16/src/main.rs
@@ -32,7 +32,7 @@ fn phase(input: &str) -> String {
         .collect()
 }
 
-fn flawed_frequency_transmission(input: &'static str, n: usize) -> String {
+fn flawed_frequency_transmission(input: &str, n: usize) -> String {
     let mut result = input.to_string();
     for _ in 0..n {
         result = phase(&result);
@@ -40,8 +40,62 @@ fn flawed_frequency_transmission(input: &'static str, n: usize) -> String {
     result
 }
 
+fn nth_eight_digits(n: usize, s: &str) -> String {
+    s.chars().skip(n).take(8).collect::<String>()
+}
+
 fn first_eight_digits(s: &str) -> String {
-    s.chars().take(8).collect::<String>()
+    nth_eight_digits(0, s)
+}
+
+fn real_fft(input: &'static str, n: usize) -> String {
+    let message_offset: usize = input.chars().take(7).collect::<String>().parse().unwrap();
+    let original_length = input.chars().count();
+    let real_length = 10_000 * original_length;
+    // If the total length is less than half the message offset, we are in a special case that is
+    // easy to optimize:
+    // Here what the pattern looks like:
+    // 0123456789abcdefghi
+    // + - + - + - + - + -
+    //  ++  --  ++  --  ++
+    //   +++   ---   +++
+    //    ++++    ----
+    //     +++++     -----
+    //      ++++++      --
+    //       +++++++
+    //        ++++++++
+    //         +++++++++
+    //          +++++++++
+    //           ++++++++
+    //            +++++++
+    //             ++++++
+    //              +++++
+    //               ++++
+    //                +++
+    //                 ++
+    //                  +
+    // In the bottom half, the problem is reduced to simply
+    // summing all the last numbers.
+    assert!(2 * message_offset > real_length);
+    let mut next = input
+        .chars()
+        .cycle()
+        .take(real_length)
+        .skip(message_offset)
+        .collect::<String>();
+    for _ in 0..n {
+        let mut sum = 0;
+        next = next
+            .chars()
+            .rev()
+            .map(|c| {
+                sum = (c.to_digit(10).unwrap() + sum) % 10;
+                std::char::from_digit(sum, 10).unwrap()
+            })
+            .collect::<String>();
+        next = next.chars().rev().collect::<String>();
+    }
+    first_eight_digits(&next)
 }
 
 fn main() {
@@ -51,6 +105,9 @@ fn main() {
     ));
     assert_eq!("18933364".to_string(), part_1);
     println!("part 1: {}", part_1);
+    let part_2 = real_fft(include_str!("input.txt").trim(), 100);
+    assert_eq!("28872305".to_string(), part_2);
+    println!("part 2: {}", part_2);
 }
 
 #[cfg(test)]
@@ -73,7 +130,20 @@ mod tests {
         let input_signal = "69317163492948606335995924319873";
         assert_eq!(
             "52432133".to_string(),
-            first_eight_digits(flawed_frequency_transmission(input_signal, 100))
+            first_eight_digits(&flawed_frequency_transmission(input_signal, 100))
         );
+    }
+    #[test]
+    fn test_real_fft() {
+        let input_signal = "03036732577212944063491565474664";
+        assert_eq!("84462026", real_fft(input_signal, 3));
+
+        // These two examples don't exhibit the property I rely on for optimizing this case, so
+        // f*ck em :)
+        // let input_signal = "02935109699940807407585447034323 ";
+        // assert_eq!("78725270", real_fft(input_signal, 3));
+
+        // let input_signal = "03081770884921959731165446850517 ";
+        // assert_eq!("53553731", real_fft(input_signal, 3));
     }
 }
