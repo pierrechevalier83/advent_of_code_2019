62e411d5d5e935df6824b4f41f5c62067ead9e8c
Pierre Chevalier <pierrechevalier83@gmail.com>
2019-12-23 18:31:00 +0000

    17: part 2: drive beebop through the scaffoldings

--- 17/src/main.rs
+++ 17/src/main.rs
@@ -1,5 +1,6 @@
 use direction::{CardinalDirection, CardinalDirectionIter, Coord};
-use intcode_computer::Computer;
+use intcode_computer::{ComputationStatus, Computer};
+use itertools::Itertools;
 use map_display::MapDisplay;
 use std::collections::HashMap;
 use std::fmt::{self, Debug, Display, Formatter};
@@ -10,6 +11,7 @@ enum TileContent {
     Empty,
     Scaffold,
     Robot,
+    Ascii(char),
 }
 
 impl Default for TileContent {
@@ -23,8 +25,8 @@ impl From<char> for TileContent {
         match c {
             '.' => TileContent::Empty,
             '#' => TileContent::Scaffold,
-            '^' => TileContent::Robot,
-            _ => panic!("Unexpected tile: '{}'", c),
+            '^' | '<' | '>' => TileContent::Robot,
+            _ => TileContent::Ascii(c),
         }
     }
 }
@@ -32,9 +34,10 @@ impl From<char> for TileContent {
 impl Display for TileContent {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         let px = match self {
-            Self::Empty => "  ",
-            Self::Scaffold => "🚧",
-            Self::Robot => "🤖",
+            Self::Empty => "  ".to_string(),
+            Self::Scaffold => "🚧".to_string(),
+            Self::Robot => "🤖".to_string(),
+            Self::Ascii(c) => format!("{}", c),
         };
         write!(f, "{}", px)
     }
@@ -51,8 +54,13 @@ impl Camera {
         for line in computer_output
             .trim()
             .split('\n')
-            .map(|s| s.parse::<u8>().unwrap())
-            .map(char::from)
+            .map(|s| {
+                if let Ok(i) = s.parse::<u8>() {
+                    format!("{}", char::from(i))
+                } else {
+                    format!("{}", s)
+                }
+            })
             .collect::<String>()
             .split('\n')
         {
@@ -121,16 +129,73 @@ impl Debug for Move {
     }
 }
 
+#[derive(Clone, Debug, Eq, PartialEq)]
+struct MovementRoutine {
+    main: String,
+    a: String,
+    b: String,
+    c: String,
+}
+
+impl MovementRoutine {
+    fn is_valid_main_char(c: char) -> bool {
+        match c {
+            'A' | 'B' | 'C' | ',' => true,
+            _ => false,
+        }
+    }
+    fn is_valid_subroutine_char(c: char) -> bool {
+        match c {
+            'L' | 'R' | ',' => true,
+            _ => c.is_digit(10),
+        }
+    }
+    fn main_is_valid(main: &str) -> bool {
+        main.len() <= 20 && main.chars().all(Self::is_valid_main_char)
+    }
+    fn subroutine_is_valid(subroutine: &str) -> bool {
+        subroutine.len() <= 20 && subroutine.chars().all(Self::is_valid_subroutine_char)
+    }
+    fn is_valid(&self) -> bool {
+        Self::main_is_valid(&self.main)
+            && Self::subroutine_is_valid(&self.a)
+            && Self::subroutine_is_valid(&self.b)
+            && Self::subroutine_is_valid(&self.c)
+    }
+    fn as_seq_str(&self) -> String {
+        self.main
+            .replace("A", &self.a)
+            .replace("B", &self.b)
+            .replace("C", &self.c)
+    }
+    fn as_ascii(s: &&String) -> String {
+        let s = format!("{}\n", s)
+            .encode_utf16()
+            .map(|code| format!("{}", code))
+            .intersperse("\n".to_string())
+            .collect();
+        s
+    }
+    fn as_computer_input(&self) -> String {
+        [&self.main, &self.a, &self.b, &self.c, &"n".to_string()]
+            .iter()
+            .map(Self::as_ascii)
+            .intersperse("\n".to_string())
+            .collect()
+    }
+}
+
 struct Robot {
     position: Coord,
     facing: CardinalDirection,
     map: HashMap<Coord, TileContent>,
 }
+
 impl Robot {
     fn new(map: HashMap<Coord, TileContent>) -> Self {
         let position = map
             .iter()
-            .find(|(k, v)| v == &&TileContent::Robot)
+            .find(|(_k, v)| v == &&TileContent::Robot)
             .map(|(k, _v)| *k)
             .unwrap();
         let facing = CardinalDirection::North;
@@ -173,7 +238,6 @@ impl Robot {
             }
             if num_steps > 0 {
                 moves.push(Move::Forward(num_steps));
-                num_steps = 0;
             }
             if self.can_turn(Turn::Left) {
                 moves.push(Move::Rotate(Turn::Left));
@@ -185,26 +249,61 @@ impl Robot {
         }
         moves
     }
+    fn break_sequence_up(sequence: &[Move]) -> MovementRoutine {
+        // Just broke the sequence by eye. Sometimes, it's easier to spot patterns by eye than with
+        // fancy algos...
+        let routine = MovementRoutine {
+            main: "A,B,A,B,C,C,B,A,B,C".to_string(),
+            a: "L,12,L,6,L,8,R,6".to_string(),
+            b: "L,8,L,8,R,4,R,6,R,6".to_string(),
+            c: "L,12,R,6,L,8".to_string(),
+        };
+
+        let seq_str = sequence
+            .iter()
+            .map(|m| format!("{:?}", m))
+            .intersperse(','.to_string())
+            .collect::<String>();
+        assert!(routine.is_valid());
+        assert_eq!(seq_str, routine.as_seq_str());
+        routine
+    }
+    fn create_computer_input_sequence(&mut self) -> String {
+        let seq = self.find_shortest_total_sequence();
+        Self::break_sequence_up(&seq).as_computer_input()
+    }
 }
 
 fn main() {
-    let mut computer = Computer::from_str(include_str!("input.txt")).unwrap();
-    computer.set_mock_io_input("");
-    computer.compute().unwrap();
-    let output = computer.get_mock_io_output().unwrap();
-    let camera = Camera::new(&output);
-    println!("{}", camera);
-    let part_1 = camera.total_alignment_parameter();
-    println!("part 1: {}", part_1);
-    let mut bot = Robot::new(camera.map.clone());
-    println!("{:?}", bot.find_shortest_total_sequence());
-    // TODO: break the sequence into routines
-    // Display it as ascii
+    {
+        let mut computer = Computer::from_str(include_str!("input.txt")).unwrap();
+        computer.set_mock_io_input("");
+        computer.compute().unwrap();
+        let output = computer.get_mock_io_output().unwrap();
+        let camera = Camera::new(&output);
+        println!("{}", camera);
 
-    // part 2:
-    // main movement routine: A, B, B, C, A
-    // where A, B and C map to a movement function
-    // Find one set of moves to go from start to end (maybe all ways to do it)
-    // Find an efficient way to break it down into 3 functions of up to 20 characters (including
-    // comas, excluding newlines)
+        let part_1 = camera.total_alignment_parameter();
+        assert_eq!(6024, part_1);
+        println!("part 1: {}", part_1);
+    }
+    {
+        let mut computer = Computer::from_str(include_str!("input.txt")).unwrap();
+        // Wake up, beebop!
+        computer.data[0] = 2;
+        computer.set_mock_io_input("");
+        computer.compute().unwrap();
+        let output = computer.get_mock_io_output().unwrap();
+        let camera = Camera::new(&output.trim());
+        let mut bot = Robot::new(camera.map.clone());
+        let input = bot.create_computer_input_sequence();
+        computer.set_mock_io_input(&input);
+        let status = computer.compute().unwrap();
+        assert_eq!(ComputationStatus::Done, status);
+        let output = computer.get_mock_io_output().unwrap();
+        let screen = format!("{}", Camera::new(&output.trim()));
+        let part_2 = screen.trim().split("\n").last().unwrap();
+        assert_eq!("897344", part_2);
+        println!("part 2: {}", part_2);
+    }
 }
